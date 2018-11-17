43e5289f718f66352631cc138dd7b734c909cc6e
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-16 20:19:13 -0500

    Early Days

---
+++ Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Pawn ControlledPawn;
+
+    public string[] Inputs;
+    private enum Action
+    {
+        JOYSTICK_HORIZONTAL,
+        JOYSTICK_VERTICAL,
+        JUMP,
+        INTERACT,
+        TOGGLE_SPRINT
+    }
+    //0: Joystick horizontal axis
+    //1: Joystick vertical axis
+    //2: Jump button
+    //3: Interact
+    //4: Toggle sprint
+
+    protected virtual void Update()
+    {
+        
+    }
+
+    protected virtual void HandleInput()
+    {
+        //Inputs[Action.JOYSTICK_HORIZONTAL]
+    }
+
+    protected virtual void PassJoystick(Vector2 value)
+    {
+
+    }
+}
