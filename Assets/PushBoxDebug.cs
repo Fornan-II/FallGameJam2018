8ce817d9cc298e6833a362bb5f278179f22cb260
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-17 15:25:32 -0500

    Pushable Box WIP

---
+++ Assets/PushBoxDebug.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBoxDebug : MonoBehaviour
+{
+    public Vector3 pushDir = Vector3.zero;
+    public bool Push = false;
+
+	void Update ()
+    {
+		if(Push)
+        {
+            PushableBox pb = GetComponent<PushableBox>();
+            if(pb)
+            {
+                Debug.DrawRay(transform.position, pushDir, Color.red, 1.0f);
+                pb.PushBox(pushDir);
+            }
+
+            Push = false;
+        }
+	}
+}
---
+++ Assets/Scripts/PushableBox.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class PushableBox : Interactable
+{
+    public float PushDistance = 1.0f;
+    protected Rigidbody _rb;
+    protected Collider _col;
+    protected bool _isBeingPushed = false;
+    public bool CanBePushed { get { return !_isBeingPushed; } }
+
+    protected Vector3 _startPosition;
+    protected Vector3 _targetPosition;
+
+
+    protected virtual void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<Collider>();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        if(_isBeingPushed)
+        {
+            HandlePushing();
+        }
+    }
+
+    public override void Interact(GameObject interacter)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool PushBox(Vector3 direction)
+    {
+        if(_isBeingPushed) { return false; }
+
+        RaycastHit hit;
+        if(_rb.SweepTest(direction, out hit, PushDistance * 0.9f, QueryTriggerInteraction.Ignore))
+        {
+            Debug.Log("Failed sweep");
+            return false;
+        }
+        else
+        {
+            float checkDistance = 0.1f;
+            Vector3 pushPosition = transform.position;
+            pushPosition.y = _col.bounds.min.y;
+            pushPosition += (direction * PushDistance);
+            Ray groundCheckRay = new Ray(pushPosition + (Vector3.up * checkDistance * 0.5f), Vector3.down);
+            Debug.DrawRay(groundCheckRay.origin, groundCheckRay.direction * checkDistance, Color.green, 1.0f);
+            if(Physics.Raycast(groundCheckRay, checkDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+            {
+                _isBeingPushed = true;
+                _startPosition = transform.position;
+                _targetPosition = transform.position + (direction * PushDistance);
+                Debug.Log("Success");
+                return true;
+            }
+            else
+            {
+                Debug.Log("Failed ground check");
+                return false;
+            }
+        }
+    }
+
+    protected virtual void HandlePushing()
+    {
+        //float distTravelled = Vector3.Distance(transform.position, _startPosition) / PushDistance;
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, 0.2f);
+        if(Vector3.Distance(transform.position, _targetPosition) < 0.1f)
+        {
+            transform.position = _targetPosition;
+            _isBeingPushed = false;
+        }
+    }
+}
