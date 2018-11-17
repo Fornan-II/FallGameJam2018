efd5f1233c8fd0cc1aa8b6badee9d6b48b0d4d2f
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-17 11:55:30 -0500

    AI state machine foundation in place

---
+++ Assets/Scripts/AI/StateMachine Core/State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class State : ScriptableObject
+{
+    public enum StatePhase
+    {
+        ENTERING,
+        ACTIVE,
+        EXITING,
+        INACTIVE
+    }
+    protected StatePhase _currentPhase = StatePhase.ENTERING;
+    public StatePhase CurrentPhase { get { return _currentPhase; } }
+
+    public abstract void OnEnter();
+
+    public abstract void ActiveBehavior();
+
+    public abstract void OnExit();
+
+    public virtual void EnterBehavior() { }
+
+    public virtual void ExitBehavior() { }
+
+    public virtual StateNode GetRequestedNextNode()
+    {
+        return null;
+    }
+}
