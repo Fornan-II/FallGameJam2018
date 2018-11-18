6889b792d2e55662600dba220708f9f98d11fb5c
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-17 21:32:49 -0500

    Setting Up Prefabs

--- Assets/Scripts/Power Mechanics/PressurePlate.cs
+++ Assets/Scripts/Power Mechanics/PressurePlate.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PressurePlate : PowerSource
 {
     public float ExtraPowerTime = 0.3f;
     protected float _powerTimeRemaining = 0.0f;
 
+    protected Animator _anim;
+
+    protected virtual void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
     protected virtual void FixedUpdate()
     {
+        _anim.SetBool("IsDepressed", GetIsPowered());
+
         if(_powerTimeRemaining > 0.0f)
         {
             _powerTimeRemaining -= Time.fixedDeltaTime;
---
+++ Assets/Scripts/yKill.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yKill : MonoBehaviour
+{
+    public float killYLevel;
+
+    protected Vector3 _startingPos;
+
+    protected virtual void Start()
+    {
+        _startingPos = transform.position;
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        if(transform.position.y < killYLevel)
+        {
+            Respawn();
+        }
+    }
+
+    public virtual void Respawn()
+    {
+        transform.position = _startingPos;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(rb)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
