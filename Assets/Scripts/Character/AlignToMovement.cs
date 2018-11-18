3469a7ff25d190a42ef6b065b0fe06f43d152a73
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-17 21:39:47 -0500

    Player Prefab Update
    4 Camera Perspectives & char model aligning to move direction.

---
+++ Assets/Scripts/Character/AlignToMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignToMovement : MonoBehaviour
+{
+    public Rigidbody TrackedRigidBody;
+    public bool TrackX = true;
+    public bool TrackY = true;
+    public bool TrackZ = true;
+
+    void Start ()
+    {
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        if(!TrackedRigidBody) { return; }
+
+        Vector3 newDirection = TrackedRigidBody.velocity.normalized;
+        if (!TrackX) { newDirection.x = 0.0f; }
+        if (!TrackY) { newDirection.y = 0.0f; }
+        if (!TrackZ) { newDirection.z = 0.0f; }
+        transform.forward = Vector3.Slerp(transform.forward, newDirection, 0.25f);
+    }
+}
