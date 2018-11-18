dadc43148131588749ab3b29e13be3a30a086a86
gr8y <37554278+gr8y@users.noreply.github.com>
2018-11-18 14:28:23 -0500

    end screen

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+    public GameObject endPanel;
+    GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            player = other.gameObject;
+
+            if (player.GetComponent<PlayerController>())
+            {
+                player.GetComponent<PlayerController>().enabled = false;
+            }
+
+            endPanel.SetActive(true);
+        }
+    }
+}
