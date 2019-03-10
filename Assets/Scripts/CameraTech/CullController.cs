1fc23a7e23f0a0f21c7c6bfe0676d17ed0925923
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-03-09 23:30:30 -0500

    Building Culling in main scene
    Kinda meh at the moment, needs to also cull the plants. Also need to make it not cull the building the player is in.

--- Assets/Scripts/CameraTech/CullController.cs
+++ Assets/Scripts/CameraTech/CullController.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class CullController : MonoBehaviour
 {
-    public Transform focusObject;
     public bool doCulling = true;
-    [Range(0.0f, 1.0f)]
-    public float cullDistance = 0.5f;
+    public float cullThickness = 1.0f;
     public LayerMask cullLayer;
 
     protected Camera _c;
@@ -19,15 +17,14 @@ public class CullController : MonoBehaviour
 
     private void Update()
     {
-        /*Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_c);
-        foreach (CullTarget ct in CullTarget.sceneTargets)
+        if(!doCulling)
         {
-            //https://answers.unity.com/questions/8003/how-can-i-know-if-a-gameobject-is-seen-by-a-partic.html
-            ct.SetVisibility(GeometryUtility.TestPlanesAABB(planes, ct.collider.bounds));
-        }*/
+            return;
+        }
 
         // https://answers.unity.com/questions/174002/what-is-the-relationship-between-camera-size-units.html
         Vector3 boxDimensions = Vector3.one;
+        boxDimensions.z = cullThickness;
         boxDimensions.y = _c.orthographicSize; //would usually mult this by 2, but overlab box takes half dimensions
         boxDimensions.x = boxDimensions.y * _c.aspect;
         Collider[] hitCol = Physics.OverlapBox(transform.position, boxDimensions, transform.rotation, cullLayer);
